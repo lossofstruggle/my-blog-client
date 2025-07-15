e75ccaa8eaaee74afb6aa73749a43d4d8ba5f1f2
qianchunliu <799273114qian@gmail.com>
2025-07-15 13:26:58 +0800

    修改nextConfig

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['picsum.photos'],
-  },
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  }
 };
 
 export default nextConfig;
